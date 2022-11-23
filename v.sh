e7d651963b0f3bea359a6e8d4630f5ef27a5e0d2
llinux910 <98379756+llinux910@users.noreply.github.com>
2022-11-23 05:45:27 +0000

    Update and rename v.sh to vir.sh

--- v.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /home/jph/proj/jupyter/virenv/bin/activate
-cd /home/jph/proj/jupyter/virenv/bin/
-jupyter notebook 
-
-
-
-
---
+++ vir.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+source /home/jph/virenv/bin/activate
+cd /home/jph/virenv/bin/
+jupyter notebook 
